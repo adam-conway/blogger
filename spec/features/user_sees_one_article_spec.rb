015c9e1bc566184a77f4d54b5e178d37863ec477
adam-conway <adam.n.conway@gmail.com>
2018-03-22 13:04:14 -0600

    Added test for show page

---
+++ spec/features/user_sees_one_article_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "user sees one article" do
+	describe "they link from the articles index" do
+		it "displays information for one article" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit '/articles'
+      click_on(article_1.title)
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+		end
+	end
+end
