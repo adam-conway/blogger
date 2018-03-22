173f5222cbe725f52b6e5abb985179b695368aea
adam-conway <adam.n.conway@gmail.com>
2018-03-22 13:10:23 -0600

    Fixing test

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 describe "user sees one article" do
 	describe "they link from the articles index" do
 		it "displays information for one article" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+  		article1 = Article.create!(title: "Title 1", body: "Body 1")
 
       visit '/articles'
-      click_on(article_1.title)
+      click_on(article1.title)
 
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article1.body)
 		end
 	end
 end
